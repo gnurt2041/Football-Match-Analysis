65786119cff4a00cdf61e01e964406f6308a72fa
Thanh Trung <71776470+gnurt2041@users.noreply.github.com>
2023-06-29 19:32:58 +0700

    Upload analysis

--- football/analysis/possession.py
+++ football/analysis/possession.py
@@ -8,11 +8,10 @@ def get_player_in_possession(
     player_detections: List[Detection],
     ball_detections: List[Detection],
     proximity: int,
-    min_distance: int = 500,
-    min_proximity: int = 5
+    min_distance: int = 500
 ) -> Optional[Detection]:
-    if len(ball_detections) != 1:
-        return None,[]
+    if len(ball_detections) < 1:
+        return None
     ball_detection = ball_detections[0]
     player_closest_to_ball = None
     for player_detection in player_detections:
@@ -22,23 +21,13 @@ def get_player_in_possession(
         if distance_to_ball < min_distance :
            player_closest_to_ball = player_detection
            min_distance = distance_to_ball
-    # if min_distance > distance_threshold :
     if player_closest_to_ball:
-            # scale_x = 0.4
-            # scale_y = 0.45
-            # right_foot_error_box, left_foot_error_box = player_closest_to_ball.rect.pad_specific(scale_x = scale_x, scale_y = scale_y)
             if player_closest_to_ball.rect.pad(proximity).contains_point(point=ball_detection.rect.center):
-                #     if get_iou(ball_detection.rect, right_foot_error_box) < 0.5 or get_iou(ball_detection.rect, left_foot_error_box) < 0.5: 
-                # if not left_foot_error_box.contains_point(point=ball_detection.rect.center) and not right_foot_error_box.contains_point(point=ball_detection.rect.center):
-                # if min_distance < proximity:
-                return player_closest_to_ball, ball_detections
+                return player_closest_to_ball
             else:
-                return None, ball_detections
-            # else:
-            #     return None, ball_detections
-    
+                return None
     else:
-      return None, ball_detections
+      return None
 
 # MCI dang co bong, bong bay qua 1 tk RMA, thi van tinh cho tim hien tai la team MCI 1 khung hinh co bong,
 # thoi gian van tan + them 1, 
@@ -60,12 +49,9 @@ def get_team_in_possession(player_possession: Detection, args, team_possession: 
             team_possession['current_team'] = args.teams_name[0]
             team_possession['true_current_team'] = args.teams_name[0]
             team_possession['counter'] = 0
-
+        team_possession[team_possession['true_current_team']] += 1
+        team_possession['duration'] += 1
         if player_possession is not None:
-           #   if team_possession['current_team'] == team_possession['true_current_team']
-           team_possession[team_possession['true_current_team']] += 1
-           team_possession['duration'] += 1
-           
            if team_possession['current_team'] != player_possession.classification :
                 team_possession['counter'] = 0
                 team_possession['current_team'] = player_possession.classification
@@ -79,7 +65,7 @@ def get_team_in_possession(player_possession: Detection, args, team_possession: 
 
 def inertia_possession(player_possession: Detection, team_possession: dict) -> Optional[Detection]:
   if player_possession:
-     if player_possession.classification == team_possession['true_current_team'] :
+     if player_possession.classification == team_possession['true_current_team']:
         return player_possession
      else:
         return None

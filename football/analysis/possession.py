834fa6d023971375fe94e633ba2bc7b2a0bc13f7
Thanh Trung <71776470+gnurt2041@users.noreply.github.com>
2023-07-02 20:20:44 +0700

    Update possession.py

--- football/analysis/possession.py
+++ football/analysis/possession.py
@@ -51,13 +51,17 @@ def get_team_in_possession(player_possession: Detection, args, team_possession: 
             team_possession['counter'] = 0
         team_possession[team_possession['true_current_team']] += 1
         team_possession['duration'] += 1
+        
+        if player_possession is None:
+            return team_possession
+            
+       if team_possession['current_team'] != player_possession.classification :
+            team_possession['counter'] = 0
+            team_possession['current_team'] = player_possession.classification
+               
         team_possession['counter'] += 1
-        if player_possession is not None:
-           if team_possession['current_team'] != player_possession.classification :
-                team_possession['counter'] = 0
-                team_possession['current_team'] = player_possession.classification
-
-           if team_possession['counter'] >= args.possession_threshold :
+    
+           if team_possession['counter'] >= args.possession_threshold and player_possession is not None :
                 team_possession['true_current_team'] = team_possession['current_team']
 
         return team_possession

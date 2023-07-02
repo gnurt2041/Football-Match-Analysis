a8ea9f6e0764f12a0e4dac3b01de93adb50f4d33
Thanh Trung <71776470+gnurt2041@users.noreply.github.com>
2023-07-02 17:18:23 +0700

    Update filter.py

--- football/analysis/filter.py
+++ football/analysis/filter.py
@@ -8,44 +8,44 @@ def filters(args):
                 if color == item['name']:
                     colors.append(item)
     else:
-        colors_t1 = []
-        for color in args.color_t1:
+        colors_home = []
+        for color in args.color_home:
             for item in all:
                 if color == item['name']:
-                    colors_t1.append(item)
-        colors_t2 = []
-        for color in args.color_t2:
+                    colors_home.append(item)
+        colors_away = []
+        for color in args.color_away:
             for item in all:
                 if color == item['name']:
-                    colors_t2.append(item)
+                    colors_away.append(item)
         colors_rf = []
         for item in all:
             if args.color_rf == item['name']:
                 colors_rf.append(item)
 
     if len(args.colors) == 0:
-        if len(colors_t1) > 1:
-            colors_t1 = list(colors_t1)
+        if len(colors_home) > 1:
+            colors_home = list(colors_home)
         else:
-            colors_t1 = colors_t1
-        if len(colors_t2) > 1:
-            colors_t2 = list(colors_t2)
+            colors_home = colors_home
+        if len(colors_away) > 1:
+            colors_away = list(colors_away)
         else:
-            colors_t2 = colors_t2
+            colors_away = colors_away
         colors_rf = colors_rf
     else:
-        colors_t1 = list(colors[:2])
-        colors_t2 = list(colors[2:4])
+        colors_home = list(colors[:2])
+        colors_away = list(colors[2:4])
         colors_rf = [colors[-1]]
 
     team_1_filter = {
         "name": args.teams_name[0],
-        "colors": colors_t1,
+        "colors": colors_home,
     }
 
     team_2_filter = {
         "name": args.teams_name[1],
-        "colors": colors_t2,
+        "colors": colors_away,
     }
 
     referee_filter = {
